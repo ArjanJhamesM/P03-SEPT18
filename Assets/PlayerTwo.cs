b89a3fdfb0960424d651f6f624f5f55a69df4f8a
PythonAndrei <sirunoandrei75@gmail.com>
2024-09-18 10:26:19 +0800

    New PLayer (2)
    DASDAS

---
+++ Assets/PlayerTwo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwo : MonoBehaviour
+{
+    public string Name = "ANDREI";
+    public int Age = 69;
+    public string Race = "lightskin black";
+    public int PhysicalDamage = 50;
+    public int Armor = 160;
+    public int MagicaDamage = 43;
+    public int MagicalResistance = 50;
+    public float speed = 80;
+}
